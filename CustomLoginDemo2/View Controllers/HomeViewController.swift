9bc47d0f5c1fab564ec29993c1b3f75851825bc9
Scott Hsu <b01610033@gmail.com>
2021-02-28 15:26:32 +0800

    user registration initial commit

---
+++ CustomLoginDemo2/View Controllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  CustomLoginDemo2
+//
+//  Created by Scott Hsu on 2021/2/7.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
