cb292670257578b343a69e00ecaf8226a9216429
Scott Hsu <b01610033@gmail.com>
2021-04-11 11:45:52 +0800

    new files

---
+++ Picoweb/src/App.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './App.css';
+import Auth from '@aws-amplify/auth';
+import Analytics from '@aws-amplify/analytics';
+import awsconfig from './aws-exports';
+import { withAuthenticator } from 'aws-amplify-react'
+
+Auth.configure(awsconfig);
+Analytics.configure(awsconfig);
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    Analytics.record('AWS Amplify Course Event');
+
+
+    Analytics.record({
+        name: 'Course',
+        attributes: { tech: 'AWS Amplify', author: 'Bear' }
+    });
+    Analytics.record({
+        name: 'Course',
+        attributes: { tech: 'AWS IAM', author: 'Bear' }
+    });
+    console.log("test");
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-intro">
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default withAuthenticator(App);
